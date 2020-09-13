770feb56a0797fd0003d5463c45fe1319e309afb
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2020-09-13 12:18:34 +0200

    Adding start and stop game rule when a game is started or stopped

--- src/main/java/fr/pederobien/minecraftrules/commands/RulesCommand.java
+++ src/main/java/fr/pederobien/minecraftrules/commands/RulesCommand.java
@@ -1,12 +1,41 @@
 package fr.pederobien.minecraftrules.commands;
 
+import java.time.LocalTime;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 import fr.pederobien.minecraftgameplateform.commands.AbstractSimpleCommand;
+import fr.pederobien.minecraftgameplateform.interfaces.element.IGame;
+import fr.pederobien.minecraftgameplateform.interfaces.runtime.timeline.IObsTimeLine;
+import fr.pederobien.minecraftgameplateform.utils.Plateform;
+import fr.pederobien.minecraftrules.impl.GameRule;
 
 public class RulesCommand extends AbstractSimpleCommand {
+	private IObsTimeLine pvpActivator;
 
 	public RulesCommand(JavaPlugin plugin) {
 		super(plugin, new RulesEdition());
+		pvpActivator = new PvpActivator();
+	}
+
+	@Override
+	public <U extends IGame> void onGameIsStarted(U IGame) {
+		Plateform.getTimeLine().addObserver(Plateform.getGameConfigurationContext().getPvpTime(), pvpActivator);
+		GameRule.RUNNABLE_RULES.forEach(rule -> rule.start());
+		GameRule.PVP.setValue(false);
+	}
+
+	@Override
+	public <U extends IGame> void onGameIsStopped(U IGame) {
+		GameRule.RUNNABLE_RULES.forEach(rule -> rule.stop());
+	}
+
+	private class PvpActivator implements IObsTimeLine {
+
+		@Override
+		public void timeChanged(LocalTime time) {
+			GameRule.PVP.setValue(true);
+			Plateform.getGameConfigurationContext().getGame().onPvpEnabled();
+		}
 	}
 }
