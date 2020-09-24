b1a19f36d7f017ec2b174c6b9929977fe73a1f2a
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2020-09-24 11:20:57 +0200

    Adding static method get in class RulesPlugin

--- src/main/java/fr/pederobien/minecraftrules/RulesPlugin.java
+++ src/main/java/fr/pederobien/minecraftrules/RulesPlugin.java
@@ -3,6 +3,7 @@ package fr.pederobien.minecraftrules;
 import java.io.FileNotFoundException;
 import java.nio.file.Path;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import fr.pederobien.dictionary.interfaces.IDictionaryParser;
@@ -11,7 +12,14 @@ import fr.pederobien.minecraftrules.commands.RulesCommand;
 import fr.pederobien.minecraftrules.impl.GameRule;
 
 public class RulesPlugin extends JavaPlugin {
-	public static String NAME = "minecraft-rules";
+	private static Plugin plugin;
+
+	/**
+	 * @return The plugin associated to this rules plugin.
+	 */
+	public static Plugin get() {
+		return plugin;
+	}
 
 	@Override
 	public void onEnable() {
@@ -37,7 +45,7 @@ public class RulesPlugin extends JavaPlugin {
 	}
 
 	private void registerDictionary(String parent, String... dictionaryNames) {
-		Path jarPath = Plateform.ROOT.getParent().resolve(NAME.concat(".jar"));
+		Path jarPath = Plateform.ROOT.getParent().resolve(getName().concat(".jar"));
 		String dictionariesFolder = "resources/dictionaries/".concat(parent).concat("/");
 		for (String name : dictionaryNames)
 			registerDictionary(Plateform.getDefaultDictionaryParser(dictionariesFolder.concat(name)), jarPath);
