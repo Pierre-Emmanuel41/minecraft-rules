8619711828cd309d01ad2447120bd8c9f35a55d8
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2022-01-02 00:27:55 +0100

    persistence: Updating signature of methods serialize and deserialize

--- src/main/java/fr/pederobien/minecraft/rules/RulesPlugin.java
+++ src/main/java/fr/pederobien/minecraft/rules/RulesPlugin.java
@@ -1,11 +1,8 @@
 package fr.pederobien.minecraft.rules;
 
-import java.io.FileNotFoundException;
-
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import fr.pederobien.dictionary.exceptions.MessageRegisteredException;
 import fr.pederobien.dictionary.impl.JarXmlDictionaryParser;
 import fr.pederobien.minecraft.dictionary.impl.MinecraftDictionaryContext;
 import fr.pederobien.utils.AsyncConsole;
@@ -30,21 +27,17 @@ public class RulesPlugin extends JavaPlugin {
 	}
 
 	private void registerDictionaries() {
-		try {
-			JarXmlDictionaryParser dictionaryParser = new JarXmlDictionaryParser(getFile().toPath());
+		JarXmlDictionaryParser dictionaryParser = new JarXmlDictionaryParser(getFile().toPath());
 
-			MinecraftDictionaryContext context = MinecraftDictionaryContext.instance();
-			String[] dictionaries = new String[] { "English.xml", "French.xml" };
-			for (String dictionary : dictionaries)
-				try {
-					context.register(dictionaryParser.parse(DICTIONARY_FOLDER.concat(dictionary)));
-				} catch (MessageRegisteredException e) {
-					AsyncConsole.print(e);
-					for (StackTraceElement element : e.getStackTrace())
-						AsyncConsole.print(element);
-				}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+		MinecraftDictionaryContext context = MinecraftDictionaryContext.instance();
+		String[] dictionaries = new String[] { "English.xml", "French.xml" };
+		for (String dictionary : dictionaries)
+			try {
+				context.register(dictionaryParser.parse(DICTIONARY_FOLDER.concat(dictionary)));
+			} catch (Exception e) {
+				AsyncConsole.print(e);
+				for (StackTraceElement element : e.getStackTrace())
+					AsyncConsole.print(element);
+			}
 	}
 }
